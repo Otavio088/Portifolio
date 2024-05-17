84f69a848a833100a1782f6aa3dc4864b871a079
Otávio Augusto <otavio208augustoribeiro@gmail.com>
2024-05-17 15:40:31 -0300

    total mudança no html, incluindo novas classes, ids e novos campos. Todos os estilos css foram modificados de alguma forma para a melhoria do portifolio. novos estilos foram adicionados para implementar uma sessao de email e splash screen quando o botao for apertado. JavaScript foi usado para realizar a tela de splash screen

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function menuHead() {
-    let menuHamburger = document.querySelector('#conteudo');
+    let menuHamburger = document.querySelector('#content');
     
     if (menuHamburger.style.display == 'block'){
         menuHamburger.style.display = 'none';
@@ -7,3 +7,16 @@ function menuHead() {
         menuHamburger.style.display = 'block';
     }
 }
+
+const splash = document.querySelector('.splash');
+const form = document.querySelector('.contact-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    splash.classList.add('display-block');
+
+    setTimeout(() => {
+        splash.classList.remove('display-block');
+    }, 2000);
+});
