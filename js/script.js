a44fcb08ab31dc49377f957649e71b0560a9b9cd
Otávio Augusto <otavio208augustoribeiro@gmail.com>
2024-05-21 14:36:40 -0300

    atualizacao no formulario de contato

--- js/script.js
+++ js/script.js
@@ -1,22 +1,77 @@
 function menuHead() {
     let menuHamburger = document.querySelector('#content');
     
-    if (menuHamburger.style.display == 'block'){
+    if (menuHamburger.style.display == 'block') {
         menuHamburger.style.display = 'none';
     }else{
         menuHamburger.style.display = 'block';
     }
 }
 
-const splash = document.querySelector('.splash');
-const form = document.querySelector('.contact-form');
+// class FormSubmit {
+//     constructor(settings) {
+//         this.settings = settings;
+//         this.form = document.querySelector(settings.form);
+//         this.formButton = document.querySelector(settings.button);
+//         if (this.form) {
+//             this.url = this.form.getAttribute("action");
+//         }
+//         this.sendForm = this.sendForm.bind(this);
+//     }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+//     displaySuccess() {
+//         this.form.innerHTML = this.settings.success;
+//     }
 
-    splash.classList.add('display-block');
+//     displayError() {
+//         this.form.innerHTML = this.settings.error;
+//     }
 
-    setTimeout(() => {
-        splash.classList.remove('display-block');
-    }, 2000);
-});
+//     getFormObject() {
+//         const formObject = {};
+//         const fields = this.form.querySelectorAll("[name]");
+//         fields.forEach((field) => {
+//             formObject[field.getAttribute("name")] = field.value;
+//         });
+//         return formObject;
+//     }
+
+//     onSubmission(event) {
+//         event.preventDefault();
+//         event.target.disabled = true;
+//         event.target.innerText = "Enviando..."
+//     }
+
+//     async sendForm(event) {
+//         this.onSubmission(event);
+//         try {
+//             await fetch(this.url, {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     Accept: "application/json",
+//                 },
+//                 body: JSON.stringify(this.getFormObject()),
+//             });
+//             this.displaySuccess();
+//         } catch (error) {
+//             this.displayError();
+//             throw new Error(error);
+//         }
+//     }
+
+//     init() {
+//         if (this.form) 
+//             this.formButton.addEventListener("click", this.sendForm);  
+//         return this;
+//     }
+// }
+
+// const formSubmit = new FormSubmit({
+//     form: "[data-form]",
+//     button: "[data-button]",
+//     success: "<h1 class='success'>Mensagem enviada ❤️</h1>",
+//     error: "<h1 class='error'>Não foi possível enviar sua mensagem...</h1>",
+// });
+
+// formSubmit.init();
